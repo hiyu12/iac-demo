3cb9754699b24e1b0ea11790cee52d319c4acb5a
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2022-09-08 19:05:15 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,1 +1,8 @@
-resource 
+resource "aws_instance" "web" {
+    ami = ami-07eaf27c7c4a884cf
+    instance_type = t2.micro
+    tags = {
+      "Name" = "terraform"
+    }
+    key_name = keyAWS.pem
+}
