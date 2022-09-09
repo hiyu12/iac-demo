cd2b07f534202088ab56c2fe4cd30ed127b2e6bb
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2022-09-09 11:17:23 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
     ami = ami-07eaf27c7c4a884cf
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     count = 2
     tags = {
       "Name" = "terraform"
